68b6c0d388a48917424c1895236ceb047edea8a8
Petar Efnushev <petar.efnushev@gmail.com>
2015-11-20 19:49:41 +0100

    modified folder structure;

--- geoimage/__init__.py
+++ geoimage/__init__.py
@@ -1,37 +1,3 @@
-from .utils import Image, GeoCode
+from .geoimage import GeoImage
 
-class GeoImage(object):
-    def __init__(self, path):
-        self.image = Image(path)
-        self.geocode = GeoCode()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type is not None:
-            print exc_type, exc_value, traceback
-            
-        self.image.close()
-        return self
-
-    @property
-    def address(self):
-        return self.geocode.fetch_address(self.image.coordinates)
-
-    @property
-    def coordinates(self):
-        return self.image.coordinates
-    
-    @property
-    def latitude(self):
-        return self.image.latitude
-
-    @property
-    def longitude(self):
-        return self.image.longitude
-
-    def __repr__(self):
-        return '{}({})'.format(self.__class__.__name__, self.image.file.name)
-
-
+__all__ = ['GeoImage']
